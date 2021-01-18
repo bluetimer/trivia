43a5a7e806de4bbc9451a84b9d4347574782e9d9
Joe Tessler <jrt@google.com>
2021-01-18 17:58:53 -0500

    Goal 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,21 @@ import React, { Component, useState } from "react";
 import "../css/App.css";
 import data from "../sample_data.json";
 
-function App() {
-  return <div className="app">Trivia!</div>;
+// ### Goal 2: Render a "Next Question" button on the screen.
+
+// - [ ] In App.jsx, create a NextQuestion component.
+// - [ ] Write the JSX to display a button for the user to click for the next question. (It will not work yet.)
+// - [ ] Render `<NextQuestion />` inside of App.
+function Question(props) {
+  return <p> {props.text} </p>;
 }
 
+function App() {
+  let currentquestion = 0;
+  return (
+    <div className="app">
+      <Question text={data[currentquestion].question.text} />
+    </div>
+  );
+}
 export default App;
