c5d7346947958120d2dc800fc3cac9b5d62759c8
Issac Teoh <teohissac@gmail.com>
2021-02-01 14:48:55 -0800

    day 2 goal 1 [on going]

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,20 +2,33 @@ import React, { Component, useState } from "react";
 import "../css/App.css";
 import data from "../sample_data.json";
 
-// ### Goal 2: Render a "Next Question" button on the screen.
+// ## Goal 1: Render the answer choices from sample_data.json on the screen.
 
-// - [x] In App.jsx, create a NextQuestion component.
-// - [ ] Write the JSX to display a button for the user to click for the next question. (It will not work yet.)
-// - [ ] Render `<NextQuestion />` inside of App.
+// - [x] In App.jsx, create an Answer component.
+// - [x] Add props to `<Answer />` with the text "Answer goes here".
+// - [x] Render `<Answer />` inside of `<Question />`.
+//  - [ ] Pass props for the answer choices into `<Question />`.
+//  - [ ] Use those props to render the Answer components inside `<Question />` to display the answer choices.
+// - [ ] Refactor to use map to map over all answer choices.
 function Question(props) {
-  return <p> {props.text} </p>;
+  return (
+    <div>
+      <p> {props.text} </p>
+      <Answer text={"answer A"} />
+      <Answer text={"answer B"} />
+      <Answer text={"answer C"} />
+    </div>
+  );
 }
 
 function App() {
   let currentquestion = 0;
   return (
     <div className="app">
-      <Question text={data[currentquestion].question.text} />
+      <Question
+        text={data[currentquestion].question.choices}
+        choices={data[currentquestion].question.choices}
+      />
       <NextQuestion />
     </div>
   );
@@ -28,4 +41,28 @@ function NextQuestion() {
     </div>
   );
 }
+function Answer(props) {
+  return (
+    <div>
+      <p>{props.text}</p>
+    </div>
+  );
+}
 export default App;
+
+// import React from "react";
+
+// function GreetByName(props) { <- Accepting and using props
+
+// return <div>Hello {props.name}!</div>;
+// }
+
+// function App() {
+// return (
+// <div className="App">
+// 		<GreetByName name="Jared" /> <- Passing a prop "name" with value "Jared"
+// </div>
+// );
+// }
+
+// export default App;
